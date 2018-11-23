934155e143748670b8d6b3880a71e7d0dcb5f7c6
Kevin Goedecke <goedecke@net-studios.de>
2018-11-23 12:05:08 +0100

    Update to latest version

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,29 +1,45 @@
+  var timeout1, timeout2;
+
 
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 20,
+    typeSpeed: 15,
     backDelay: 700,
     autoInsertCss: true,
     onComplete: function(self) { 
-    // 	document.getElementById("main-window").classList.add('fade-out-slide-out');
-    // 	setTimeout(function () {
-    //         document.getElementById("main-window-answer").classList.remove('fade-out');
-    //         setTimeout(function () {
-    //         	document.getElementById("preview").classList.remove('fade-out');
-    //         	setTimeout(function () {
-    //         		document.getElementById("preview").classList.add('fade-out');
-    //         	}, 2000);
-    //         }, 2000);
-    // }, 2000);
+    	document.getElementById("main-window").classList.toggle('fade-out-slide-out');
+    	timeout1 = setTimeout(function () {
+            document.getElementById("main-window-answer").classList.toggle('fade-out');
+            timeout2 = setTimeout(function () {
+            	document.getElementById("preview").classList.toggle('fade-out');
+            	// setTimeout(function () {
+            	// 	document.getElementById("preview").classList.add('fade-out');
+            	// }, 2000);
+            }, 2000);
+    }, 2000);
     	
      },
   });
 
   document.getElementById("replay").onclick = function() {
-  	document.getElementById("main-window-answer").classList.add('fade-out');
+    if (timeout1) {
+      clearTimeout(timeout1);  
+    }
+    if (timeout2) {
+      clearTimeout(timeout2);  
+    }
+  
+  	document.getElementById("main-window-answer").classList('fade-out');
   	document.getElementById("main-window").classList.remove('fade-out-slide-out');
-  	typed.reset(true);
+
   };
 
+
+  document.getElementById("show-pdf-preview").onclick = function() {
+    document.getElementById("preview").classList.toggle('fade-out');
+    document.getElementById("preview").onclick = function() {
+      event.stopPropagation();
+    }
+  }
 
 
